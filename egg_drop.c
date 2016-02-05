f164dbde1ad9bffde3fb6d81f8cb72de40c28fa0
bartmnz <bartmnz@gmail.com>
2016-02-05 13:48:34 +0000

    implemented main function

--- egg_drop.c
+++ egg_drop.c
@@ -1,8 +1,29 @@
-#include "egg.h
 
+#include "egg.h"
+#include <stdio.h>
 /*TODO
  *
  */
+void run(int floors, int eggs){
+    if ( floors < 0 || eggs < 0){
+        return;
+    }
+}
+ 
+ 
+/* Program execution begins here
+ *
+ */
 int main(int argc, char *argv[]){
+    if ( argc != 3 ){
+        fprintf(stderr, "ERROR: useage is:   egg_drop   NUMBER_OF_FLOORS      NUMBER_OF_EGGS");
+        exit(0);
+    }
+    int floors, eggs;
+    if (!(floors = strtol(argv[0], NULL, 10)) || (eggs = strtol(argv[1], NULL, 10)) ){
+        fprintf(stderr, "ERROR: number of floors and number of eggs must be an integer greater than zero!");
+        exit(0);
+    }
+    run(floors, eggs);
     
 }
